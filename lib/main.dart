85c26a9f40ef0c5c5583f8a0a1e3d69145a13779
Nazaire N'DOUFFOU <nazairendouffou@192.168.1.4>
2024-05-23 11:53:43 +0000

    Detach HotelCard in another class

--- lib/main.dart
+++ lib/main.dart
@@ -233,155 +233,162 @@ class HotelsSection extends StatelessWidget {
             ),
           ),
           Column(
-            children: hotelList.map((hotel) {
-              return Container(
-                margin: EdgeInsets.all(10),
-                height: 230,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.all(Radius.circular(18)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade200,
-                          spreadRadius: 4,
-                          blurRadius: 6,
-                          offset: const Offset(0, 3))
-                    ]),
-                child: Column(
+              children: hotelList.map((hotel) {
+            return HotelCard(hotel);
+          }).toList())
+        ],
+      ),
+    );
+  }
+}
+
+class HotelCard extends StatelessWidget {
+  final Map hotelData;
+  HotelCard(this.hotelData);
+
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      height: 230,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(18)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.shade200,
+                spreadRadius: 4,
+                blurRadius: 6,
+                offset: const Offset(0, 3))
+          ]),
+      child: Column(
+        children: [
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(18), topRight: Radius.circular(18)),
+              image: DecorationImage(
+                  image: AssetImage(hotelData['picture']), fit: BoxFit.cover),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                    top: 5,
+                    right: -15,
+                    child: MaterialButton(
+                      color: Colors.white,
+                      shape: CircleBorder(),
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.favorite_border_rounded,
+                        color: d_green,
+                        size: 20,
+                      ),
+                    ))
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    hotelData['title'],
+                    style: GoogleFonts.nunito(
+                        fontSize: 18, fontWeight: FontWeight.w800),
+                  ),
+                  Text(
+                    '\$' + hotelData['price'],
+                    style: GoogleFonts.nunito(
+                        fontSize: 18, fontWeight: FontWeight.w800),
+                  )
+                ]),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    hotelData['place'],
+                    style: GoogleFonts.nunito(
+                        color: Colors.grey[500],
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.place,
+                        color: d_green,
+                        size: 14.0,
+                      ),
+                      Text(
+                        hotelData['distance'].toString() + 'km',
+                        style: GoogleFonts.nunito(
+                            color: Colors.grey[500],
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400),
+                      )
+                    ],
+                  ),
+                  Text(
+                    'par nuit',
+                    style: GoogleFonts.nunito(
+                        color: Colors.grey[500],
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400),
+                  )
+                ]),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 3, 10, 0),
+            child: Row(
+              children: [
+                const Row(
                   children: [
-                    Container(
-                      height: 140,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(18),
-                            topRight: Radius.circular(18)),
-                        image: DecorationImage(
-                            image: AssetImage(hotel['picture']),
-                            fit: BoxFit.cover),
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                              top: 5,
-                              right: -15,
-                              child: MaterialButton(
-                                color: Colors.white,
-                                shape: CircleBorder(),
-                                onPressed: () {},
-                                child: Icon(
-                                  Icons.favorite_border_rounded,
-                                  color: d_green,
-                                  size: 20,
-                                ),
-                              ))
-                        ],
-                      ),
+                    Icon(
+                      Icons.star_rate,
+                      color: d_green,
+                      size: 14,
                     ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              hotel['title'],
-                              style: GoogleFonts.nunito(
-                                  fontSize: 18, fontWeight: FontWeight.w800),
-                            ),
-                            Text(
-                              '\$' + hotel['price'],
-                              style: GoogleFonts.nunito(
-                                  fontSize: 18, fontWeight: FontWeight.w800),
-                            )
-                          ]),
+                    Icon(
+                      Icons.star_rate,
+                      color: d_green,
+                      size: 14,
                     ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              hotel['place'],
-                              style: GoogleFonts.nunito(
-                                  color: Colors.grey[500],
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.place,
-                                  color: d_green,
-                                  size: 14.0,
-                                ),
-                                Text(
-                                  hotel['distance'].toString() + 'km',
-                                  style: GoogleFonts.nunito(
-                                      color: Colors.grey[500],
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400),
-                                )
-                              ],
-                            ),
-                            Text(
-                              'par nuit',
-                              style: GoogleFonts.nunito(
-                                  color: Colors.grey[500],
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400),
-                            )
-                          ]),
+                    Icon(
+                      Icons.star_rate,
+                      color: d_green,
+                      size: 14,
                     ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(10, 3, 10, 0),
-                      child: Row(
-                        children: [
-                          const Row(
-                            children: [
-                              Icon(
-                                Icons.star_rate,
-                                color: d_green,
-                                size: 14,
-                              ),
-                              Icon(
-                                Icons.star_rate,
-                                color: d_green,
-                                size: 14,
-                              ),
-                              Icon(
-                                Icons.star_rate,
-                                color: d_green,
-                                size: 14,
-                              ),
-                              Icon(
-                                Icons.star_rate,
-                                color: d_green,
-                                size: 14,
-                              ),
-                              Icon(
-                                Icons.star_border,
-                                color: d_green,
-                                size: 14,
-                              )
-                            ],
-                          ),
-                          SizedBox(width: 20),
-                          Text(
-                            hotel['review'].toString() + 'reviews',
-                            style: GoogleFonts.nunito(
-                                color: Colors.grey[500],
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
+                    Icon(
+                      Icons.star_rate,
+                      color: d_green,
+                      size: 14,
+                    ),
+                    Icon(
+                      Icons.star_border,
+                      color: d_green,
+                      size: 14,
                     )
                   ],
                 ),
-              );
-            }).toList(),
+                SizedBox(width: 20),
+                Text(
+                  hotelData['review'].toString() + 'reviews',
+                  style: GoogleFonts.nunito(
+                      color: Colors.grey[500],
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400),
+                )
+              ],
+            ),
           )
         ],
       ),
     );
+    ;
   }
 }
